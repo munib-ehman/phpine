16daba49f95701aedf7850dfc99e638572c232d7
muneeb786-pixel <muneeburr451@gmail.com>
2024-02-17 17:51:15 +0500

    Add custom Routes in php and dispatch route to specific controller

---
+++ src/App/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+
+class HomeController
+{
+    public function home()
+    {
+        echo 'All the way in Home Controller';
+    }
+}
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -1,11 +1,16 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
 include __DIR__ . '/../../vendor/autoload.php';
 
+use App\Controllers\HomeController;
 use Framework\App;
 
 $app = new App();
+
+$app->get('/', [HomeController::class, 'home']);
+$app->get('about/team', [HomeController::class, 'home']);
+
 
 return $app;
