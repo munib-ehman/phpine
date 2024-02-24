7f192f5104b4703fb1640d44286376d6a9ae417e
muneeb786-pixel <muneeburr451@gmail.com>
2024-02-24 23:09:13 +0500

    setup Database connection file and add env variables in project

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -10,6 +10,10 @@ use Framework\App;
 use function App\Config\registerMiddleware;
 use function App\Config\registerRoute;
 use App\Config\Paths;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
 
 $app = new App(Paths::SOURCE . 'App/container-defination.php');
 
--- src/App/container-defination.php
+++ src/App/container-defination.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 use App\Config\Paths;
 use App\Services\ValidatorService;
+use Framework\Database;
 use Framework\TemplateEngine;
 
 return [
     TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
     ValidatorService::class => fn () => new ValidatorService(),
+    Database::class => fn () => new Database($_ENV['DB_DRIVER'], [
+        'host' => $_ENV['DB_HOST'],
+        'port' => $_ENV['DB_PORT'],
+        'dbname' => $_ENV['DB_NAME'],
+    ], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'])
 ];
